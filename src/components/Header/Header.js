237dd5b37580ddbc8575387850cefaa88b4c6729
Frank Dominguez <frankd1997@gmail.com>
2018-06-14 20:35:48 -0500

    media queries

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 const Header = props => (
     <header className="header">
         <h1 id="shadow">Pokémon Click Quest!</h1>
-        <h2 id="shadow">Click on an image to earn points, but don't click on any more than once!</h2>
+        <h2 id="shadow2">Click on an image to earn points, but don't click on any more than once!</h2>
     </header>
 );
 
