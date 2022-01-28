00488ba0c543208a9896c12296c06876064c07c9
fdominguez2 <fdominguez@naic.org>
2022-01-28 15:54:19 -0600

    Updates

---
+++ precache-manifest.6937151a1c05b63f3baaa0b6970d4c9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5713f74989716a3778d655f79d98d83b",
+    "url": "/Pokemon-Click-Quest/index.html"
+  },
+  {
+    "revision": "9887c5714eb7d373df7d",
+    "url": "/Pokemon-Click-Quest/static/css/main.eec7fead.chunk.css"
+  },
+  {
+    "revision": "40fe84f6a3e1ab82f1ea",
+    "url": "/Pokemon-Click-Quest/static/js/2.aeb7bba2.chunk.js"
+  },
+  {
+    "revision": "9887c5714eb7d373df7d",
+    "url": "/Pokemon-Click-Quest/static/js/main.f6d0a1c3.chunk.js"
+  },
+  {
+    "revision": "d94ecced2aa7f149df42",
+    "url": "/Pokemon-Click-Quest/static/js/runtime~main.334e0a52.js"
+  },
+  {
+    "revision": "831e1f4da157d96bc996f8c9f5f1e578",
+    "url": "/Pokemon-Click-Quest/static/media/PokemonGB.831e1f4d.ttf"
+  }
+]);
