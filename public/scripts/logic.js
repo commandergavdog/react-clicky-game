96f896ba1c483ba33368e6aa1b4acf3e461bb062
fdominguez2 <fdominguez@naic.org>
2023-03-23 18:36:40 -0500

    added script to responsively adjust hero top margin relative to nav height

--- public/scripts/logic.js
+++ public/scripts/logic.js
@@ -1,18 +1,15 @@
-document.onload = function () {
+window.onload = function () {
 	let nav = document.querySelector(".nav");
-	let navHeight = window.getComputedStyle(nav).height;
-			
+	let navHeight = window.getComputedStyle(nav).height;			
 	let header = document.querySelector(".header");
-
 
 	header.style.marginTop = navHeight;
 };
 
 window.onresize = function() {
 	let nav = document.querySelector(".nav");
-	let navHeight = window.getComputedStyle(nav).height;
-			
+	let navHeight = window.getComputedStyle(nav).height;			
 	let header = document.querySelector(".header");
-	
-};
 
+	header.style.marginTop = navHeight;
+};
