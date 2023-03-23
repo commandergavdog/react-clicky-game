b52e99c29e6cf1adf0dda0da0e0ae0e0804144ea
fdominguez2 <fdominguez@naic.org>
2023-03-23 18:30:43 -0500

    downloaded card images, backgrounds, and nav logo, modified image paths

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,7 +3,7 @@ import React from "react";
 const Navbar = props => (
     <div className="row">
         <ul className="nav nav-pills nav-justified">
-            <li className="col"><a href="https://commandergavdog.github.io/react-clicky-game/"><img className="logo mr-3" alt="pokemon-logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/1200px-International_Pok%C3%A9mon_logo.svg.png" />Memory Game!</a></li>
+            <li className="col"><a href="/"><img className="logo mr-3" alt="pokemon-logo" src="/images/pokemon-logo.png" />Memory Game!</a></li>
             <li className="col mt-3">{props.message || "Click an image to begin!"}</li>
             <li className="col mt-3">Score: {props.currentScore || 0} | Top Score: {props.topScore}</li>
         </ul>
