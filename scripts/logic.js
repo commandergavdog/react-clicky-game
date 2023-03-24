295c1be6934c0969d7c9f8966e96f14869e9ee80
fdominguez2 <fdominguez@naic.org>
2023-03-23 19:17:31 -0500

    Updates

---
+++ scripts/logic.js
@@ -0,0 +1,15 @@
+window.onload = function () {
+	let nav = document.querySelector(".nav");
+	let navHeight = window.getComputedStyle(nav).height;			
+	let header = document.querySelector(".header");
+
+	header.style.marginTop = navHeight;
+};
+
+window.onresize = function() {
+	let nav = document.querySelector(".nav");
+	let navHeight = window.getComputedStyle(nav).height;			
+	let header = document.querySelector(".header");
+
+	header.style.marginTop = navHeight;
+};
