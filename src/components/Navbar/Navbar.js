95db0e3be46083ed55a760ec3fd982e3278b252d
fdominguez2 <fdominguez@naic.org>
2023-03-23 19:34:02 -0500

    update image paths

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,7 +3,7 @@ import React from "react";
 const Navbar = props => (
     <div className="row">
         <ul className="nav nav-pills nav-justified">
-            <li className="col"><a href="/"><img className="logo mr-3" alt="pokemon-logo" src="/images/pokemon-logo.png" />Memory Game!</a></li>
+            <li className="col"><a href="/"><img className="logo mr-3" alt="pokemon-logo" src="./images/pokemon-logo.png" />Memory Game!</a></li>
             <li className="col mt-3">{props.message || "Click an image to begin!"}</li>
             <li className="col mt-3">Score: {props.currentScore || 0} | Top Score: {props.topScore}</li>
         </ul>
